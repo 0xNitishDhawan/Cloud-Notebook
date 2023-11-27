b9fea0ce56db29caf9d3b09d4917c966ccdcd4fb
Nitish <E21CSEU0455@bennett.edu.in>
2023-11-28 02:46:54 +0530

    (Feat.) Storing data in Database, data validation using express validator.

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,14 +1,20 @@
 const express=require('express')
 const User=require("../models/Users")
 const router=express.Router()
+const { body, validationResult } = require('express-validator');
 
-router.post('/',(req,res)=>{
-    
-    console.log(req.body);
-    const user=User(req.body);
-    user.save();
-    res.send(req.body)
-
+router.post('/',[
+    body('name', "Enter a valid name").isLength({min:3}), 
+    body('email', "Enter a valid Email").isEmail(), 
+    body('password', "Password must be 5 characters long").isLength({min:5})
+],(req,res)=>{
+    const errors = validationResult(req); 
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    User.create({name:req.body.name, email: req.body.email, password: req.body.password})
+    .then(user =>{res.json(user)})
+    .catch(err=>{res.json({error:"Please enter a unique value for Email", message:err.message})}); 
 }) 
 
 module.exports=router;  
