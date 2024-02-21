2a0da1d7494bce14dace207b8b2aee1478518dfe
Nitish <E21CSEU0455@bennett.edu.in>
2024-02-21 19:33:50 +0530

    (Feat.) Created Get UserData Endpoint, Integrated Middleware

--- Backend/db.js
+++ Backend/db.js
@@ -5,12 +5,14 @@ dotenv.config();
 const mongoURI=`mongodb+srv://studyvideos789:${process.env.MongoDBPassword}@cluster0.5fuucpl.mongodb.net/?retryWrites=true&w=majority`
 
 const connectToMongo = async () => { 
-    await mongoose.connect(mongoURI);
-    console.log("Connected to Mongo successfully");
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected to Mongo successfully");
+    } catch (error) {
+        console.log(error)        
+    }
 }
-
 module.exports=connectToMongo;       
 
-
 // const mongoURI="mongodb://localhost:27017/test"
-// await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+// await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });   
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -5,7 +5,9 @@ const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const jwtSecretKey="abcd$123@1"
+const fetchuser=require('../middleware/fetchuser')
 
+//ROUTE 1: For CREATING the User
 router.post('/createuser',[
     body('name', "Enter a valid name").isLength({min:3}), 
     body('email', "Enter a valid Email").isEmail(), 
@@ -16,32 +18,33 @@ router.post('/createuser',[
         if(!errors.isEmpty()){
             return res.status(400).json({errors:errors.array()})
         }
-
-        const salt = bcrypt.genSaltSync(10);
+        
+        const salt = await bcrypt.genSaltSync(10);
         const secretPassword= await bcrypt.hash(req.body.password, salt);
-
+        
         let user= await User.findOne({email:req.body.email})
         if(user){
             return res.status(400).send({msg:"User already exists"}); 
         }
         user= await User.create({name:req.body.name, email: req.body.email, password: secretPassword})
         const data={
-            user:{
-                id:User.id
+            user:{ 
+                id:user.id
             }
-        }
+        } 
         var authtoken = jwt.sign(data, jwtSecretKey);
- 
-        // .then(user =>{res.json(user)})
-        // .catch(err=>{res.json({error:"Please enter a unique value for Email", message:err.message})}); 
-        res.json({authtoken});  
+        
+        res.json({authtoken});   
     } 
     catch (error) {
         console.error(error);
         res.status(500).send("There is some Internal Server Error");
-    } 
+    }
 }) 
 
+
+
+//ROUTE 2: For LOGGING IN the user 
 router.post('/login',[
     body('email', "Enter a valid Email").isEmail(), 
     body('password', "Password cannot be a blank").exists()
@@ -65,11 +68,25 @@ router.post('/login',[
 
         const data={
             user:{
-                id:User.id
+                id:user.id
             }
         }
         var authtoken = jwt.sign(data, jwtSecretKey);
         res.json({authtoken});  
+    } 
+    catch (error) {
+        console.error(error);
+        res.status(500).send("There is some Internal Server Error");
+    } 
+}) 
+
+
+//ROUTE 3: For Getting the DATA of the user 
+router.post('/getuser', fetchuser,async (req,res)=>{
+    try {  
+        const userID=req.user.id;
+        const user=await User.findById(userID).select("-password");
+        res.send(user);          
     } 
     catch (error) {
         console.error(error);
